6fb493492d2c5f23de6d2b0fa893573d4b01e17d
Álvaro Pérez <alvarojperezperez@gmail.com>
2024-05-24 15:26:46 +0200

    Revert "Add health check endpoint"
    This reverts commit 181c2fcfa60961a0a65571ff4d01b3b2e33f49af.

--- routes.go
+++ routes.go
@@ -6,12 +6,7 @@ import (
 )
 
 func (router *router) setupRoutes() {
-	router.HandleFunc("/health", healthCheckHandler)
 	router.HandleFunc("/read", readMessageHandler)
-}
-
-func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
 }
 
 func readMessageHandler(w http.ResponseWriter, r *http.Request) {
